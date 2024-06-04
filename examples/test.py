468719f3ebe7c48d2de7d1c26c9ac2f89c393683
chauncygu <gshangd@163.com>
2024-06-03 17:11:13 -0700

    Update test.py

--- examples/test.py
+++ examples/test.py
@@ -6,13 +6,11 @@ import time
 from datetime import datetime
 currentDateAndTime = datetime.now()
 start_run_date_and_time=time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
-
 from robust_gymnasium.configs.robust_setting import get_config
 args = get_config().parse_args()
+args.env_name = "Ant-v4"
 
-# env = get_env_mujoco_config(args)
-
-folder = os.getcwd()[:0] + 'data/' + str(args.noise_type)+'/'+ str(
+folder = os.getcwd()[:0] + 'data/' + str(args.env_name)+'/'+ str(args.noise_type)+'/'+ str(
     start_run_date_and_time) + '/'
 print("folder---:", folder)
 # folder = os.getcwd()[:-4] + 'runs\\test\\'
@@ -27,16 +25,16 @@ with open(json_path, 'w') as f:
     f.writelines('------------------- end -------------------')
 
 # env = gym.make("Ant-v4")
+# env = gym.make(args.env_name, args, render_mode="human") # render environments
 env = gym.make(args.env_name, args)
 print("type-----------args:", args)
 
 observation, info = env.reset(seed=42)
-for i in range(10):
-    print("test----------:", i)
+for i in range(1000):
     action = env.action_space.sample()
     observation, reward, terminated, truncated, info = env.step(action)
     print("reward----------:", reward)
-
+    # env.render() # render environments
     if terminated or truncated:
         observation, info = env.reset()
 env.close()
